698bd12b5206e8243203af9753b8c9b38cec8962
k5jp1015 <k5.magami@gmail.com>
2018-09-17 13:48:46 +0900

    tutorial2 FullName

--- data/html/main.js
+++ data/html/main.js
@@ -2,24 +2,30 @@ const app = new Vue({
     el: "#app",
     data: {
       bobby: {
-        name: "Bobby Boone",
+        name: "Bobby",
+        last:"Booen",
         age: 25
       },
       john: {
-        name: "John Boby",
+        name: "John",
+        last:"Body",
         age: 35,
       },
-      hi: {
-        word: "hello"
-      }
+    },
+    computed: {
+        bobbyFullName() {
+            return `${this.bobby.first} ${this.bobby.last}`
+        },
+        johnFullName() {
+            return `${this.john.first} ${this.john.last}`
+        }
     },
     template: `
       <div>
-        <h2>Name: {{john.name}}</h2>
+        <h2>Name: {{johnFullName}}</h2>
         <h2>Age: {{john.age}}</h2>
-        <h2>Name: {{bobby.name}}</h2>
+        <h2>Name: {{bobbyFullName}}</h2>
         <h2>Age: {{bobby.age}}</h2>
-        <h2>{{hi.word}}</h2>
       </div>
     `
   })
